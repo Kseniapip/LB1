af3a9ec7c98b23a40a95b1e0b0c0c58ad00ea3e8
Ksenia Urbanovich <117678638+hyhmaw@users.noreply.github.com>
2024-10-12 09:38:20 +0300

    Update ЛБ1 Завдання 5.1.cpp

--- ЛБ1 Завдання 5.1/ЛБ1 Завдання 5.1/ЛБ1 Завдання 5.1.cpp
+++ ЛБ1 Завдання 5.1/ЛБ1 Завдання 5.1/ЛБ1 Завдання 5.1.cpp
@@ -5,27 +5,27 @@
 int main() {
     int grade;
 
-    // Введення оцінки студента
+   
     cout << "Enter student's grade (0 to 100): ";
     cin >> grade;
 
-    // Перевірка, чи оцінка в межах від 0 до 100
+
     if (grade < 0 || grade > 100) {
         cout << "Invalid grade! Please enter a number between 0 and 100." << endl;
     }
     else {
-        // Оцінювання за шкалою
+        
         if (grade >= 90) {
-            cout << "Excellent" << endl; // Відмінно
+            cout << "Excellent" << endl; 
         }
         else if (grade >= 70 && grade >= 80) {
-            cout << "Good" << endl; // Добре
+            cout << "Good" << endl;
         }
         else if (grade >= 60) {
-            cout << "Satisfactory" << endl; // Задовільно
+            cout << "Satisfactory" << endl;
         }
         else {
-            cout << "Unsatisfactory" << endl; // Незадовільно
+            cout << "Unsatisfactory" << endl; 
         }
     }
 
