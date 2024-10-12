57d6c6ca84a61d5c6bdcc9641acaa9d19ef85b39
Ksenia Urbanovich <117678638+hyhmaw@users.noreply.github.com>
2024-10-12 09:34:38 +0300

    Update ЛБ1 Завдання 3.1.cpp

--- ЛБ1 Завдання 3.1/ЛБ1 Завдання 3.1/ЛБ1 Завдання 3.1.cpp
+++ ЛБ1 Завдання 3.1/ЛБ1 Завдання 3.1/ЛБ1 Завдання 3.1.cpp
@@ -12,21 +12,21 @@ int main() {
     cout << "Enter an number:  ";
     cin >> number;
 
-    // Початкове значення числа
+   
     cout << "Initial value: " << number << endl;
 
-    // Постфіксна інкрементація (спочатку використовуємо значення, потім збільшуємо)
+   
     cout << "Postfix increment: " << number++ << endl;
     cout << "After postfix increment: " << number << endl;
 
-    // Префіксна інкрементація (спочатку збільшуємо, потім використовуємо значення)
+   
     cout << "Prefix increment: " << ++number << endl;
 
-    // Постфіксна декрементація (спочатку використовуємо значення, потім зменшуємо)
+    
     cout << "Postfix decrement: " << number-- << endl;
     cout << "After postfix decrement: " << number << endl;
 
-    // Префіксна декрементація (спочатку зменшуємо, потім використовуємо значення)
+   
     cout << "Prefix decrement: " << --number << endl;
 
     return 0;
